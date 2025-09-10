33ab98ef59d642e35ab29c4875ad5d21682d1ec0
gustav485 <gustavdambo@hotmail.dk>
2025-09-10 11:21:40 +0200

    Hello moth

--- src/file.c
+++ src/file.c
@@ -7,7 +7,7 @@ int print_hello_world(void) {
     return fprintf(stdout, "Hello, worlddfghjkl!\n");
 }
 
-/*int openFile(void) {
+int openFile(void) {
     FILE *fptr;
     fptr = fopen("test_files/ascii.input", "r");
     if (fptr==NULL){
@@ -18,25 +18,16 @@ int print_hello_world(void) {
     fprintf(stdout,"%s\n", stringStore);
     fclose(fptr);
     return 1;
-}*/
+}
 
 int main(void) {
-    FILE *fptr;
-    fptr = fopen("test_files/ascii.input", "r");
-    if (fptr==NULL){
-        fprintf(stdout,"Not able to open the file\n");
-    }
-    char stringStore[100];
-    fgets(stringStore, 100, fptr);
-    fprintf(stdout,"%s", stringStore);
-    fclose(fptr);
-    return 1;
-    /*
     int retval = EXIT_SUCCESS;
-
+    if (openFile() >= 0) {
+      return 1;
+    }
     if (print_hello_world() <= 0) {
         retval = EXIT_FAILURE;
     }
 
-    return retval;*/
+    return retval;
 }
