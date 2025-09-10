2a7d30a6be47da91cdd5351b0c1d7e5aeb6cefb8
gustav485 <gustavdambo@hotmail.dk>
2025-09-10 11:11:35 +0200

    suicide

---
+++ src/file.c
@@ -0,0 +1,42 @@
+#include <stdio.h>  // fprintf, stdout, stderr.
+#include <stdlib.h> // exit, EXIT_FAILURE, EXIT_SUCCESS.
+#include <string.h> // strerror.
+#include <errno.h>  // errno.
+
+int print_hello_world(void) {
+    return fprintf(stdout, "Hello, worlddfghjkl!\n");
+}
+
+/*int openFile(void) {
+    FILE *fptr;
+    fptr = fopen("test_files/ascii.input", "r");
+    if (fptr==NULL){
+        fprintf(stdout,"Not able to open the file\n");
+    }
+    char stringStore[100];
+    fgets(stringStore, 100, fptr);
+    fprintf(stdout,"%s\n", stringStore);
+    fclose(fptr);
+    return 1;
+}*/
+
+int main(void) {
+    FILE *fptr;
+    fptr = fopen("test_files/ascii.input", "r");
+    if (fptr==NULL){
+        fprintf(stdout,"Not able to open the file\n");
+    }
+    char stringStore[100];
+    fgets(stringStore, 100, fptr);
+    fprintf(stdout,"%s", stringStore);
+    fclose(fptr);
+    return 1;
+    /*
+    int retval = EXIT_SUCCESS;
+
+    if (print_hello_world() <= 0) {
+        retval = EXIT_FAILURE;
+    }
+
+    return retval;*/
+}
